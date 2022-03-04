795e72ad1ae590c91f7cbea902ea5fd6560e4599
BananaBros2 <98326470+BananaBros2@users.noreply.github.com>
2022-03-04 12:35:52 +0000

    4/3/20 changes

--- RogueLite Game/Assets/Scripts/Warp.cs
+++ RogueLite Game/Assets/Scripts/Warp.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Warp : Collidable
 {
+    public float XTeleport = 0;
+    public float YTeleport = 0;
+
+    public GameObject Player;
+
     protected override void OnCollide(Collider2D coll)
     {
         if (coll.name == "Player")
         {
-            //Come back later
+            Debug.Log(transform.position.x + XTeleport);
+            Player.transform.position = new Vector3((transform.position.x + XTeleport), (transform.position.y + YTeleport), 0);
+            Debug.Log(transform.position.y);
         }
     }
 }
